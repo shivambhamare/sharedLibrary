16d6ad470f3aaf0282cd6d4c481d8c065895d595
shivambhamare <147164379+shivambhamare@users.noreply.github.com>
2024-09-04 17:39:20 +0530

    Update Resource.groovy

--- vars/Resource.groovy
+++ vars/Resource.groovy
@@ -1,7 +1,7 @@
 def call()
 {
   def content = libraryResource "bash.sh"
-  writeFile file: "bash_Script.sh" , text: content
-  sh "ls -a bash_Script.sh"
-  sh "bash ./bash_Script.sh"
+    writeFile file: "bash_Script.sh" , text: content
+    sh "ls -a bash_Script.sh"
+    sh "bash ./bash_Script.sh"
 }
