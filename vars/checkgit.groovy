9665fe6105557ab036b0e15bca4c3ef0b5f5f82c
shivambhamare <147164379+shivambhamare@users.noreply.github.com>
2024-09-04 16:20:53 +0530

    Update checkgit.groovy

--- vars/checkgit.groovy
+++ vars/checkgit.groovy
@@ -1,4 +1,7 @@
-def call()
-{
-  sh 'checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[url: 'https://github.com/shivambhamare/CICD']]])'
+def call() {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: '*/main']],
+        userRemoteConfigs: [[url: 'https://github.com/shivambhamare/CICD']]
+    ])
 }
